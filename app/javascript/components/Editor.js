f059e0f9281cf5c18fd3c97ac17b252d79ab3e12
Yuppy <yuppymam@gmail.com>
2023-08-14 01:00:06 +0900

    Add event form

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import EventList from './EventList';
+import { Routes, Route } from 'react-router-dom';
+import Event from './Event';
+import EventForm from './EventForm';
 
 const Editor = () => {
   const [events, setEvents] = useState([]);
@@ -28,9 +31,21 @@ const Editor = () => {
   return (
     <>
       <Header />
-      {isError && <p>Something went wrong. Check the console.</p>}
+      <div className="grid">
+        {isError && <p>Something went wrong. Check the console.</p>}
+        {isLoading ? (
+          <p className='loading'>Loading...</p>
+        ) : (
+          <>
+            <EventList events={events} />
 
-      {isLoading ? <p>Loading...</p> : <EventList events={events} />}
+            <Routes>
+              <Route path="new" element={<EventForm />} />
+              <Route path=":id" element={<Event events={events} />} />
+            </Routes>
+          </>
+        )}
+      </div>
     </>
   );
 };
